fdf673fc3509ca746982e63db520ac26fba22e95
Kseniia-2024 <kgushchina@connect.ust.hk>
2026-05-16 15:55:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.title("Hong Kong University of Science and Technology 2026")
 st.subheader("Marks Spring Sem")
 st.write("Want predict you final GPA?")
-wanted_GPA=st.number_input(min = 1.5,
+wanted_GPA=st.number_input("Enter the targeted GPA: ",
+                           min = 1.5,
                            max=4.3,
                            value=3.0)
 
