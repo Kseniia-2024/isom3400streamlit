5d01ae931d1cc418479054b2c1566eca8cacad9d
Kseniia-2024 <kgushchina@connect.ust.hk>
2026-05-16 16:22:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,38 +1,50 @@
 import streamlit as st
 
-st.title("Hong Kong University of Science and Technology 2026")
-st.subheader("Marks Spring Sem")
-st.write("Want predict you final GPA?")
-wanted_GPA=st.number_input("Enter the targeted GPA: ",
+st.title("Hong Kong University of Science and Technology")
+st.header("Common Core Calculator Planner")
+st.subheader("For BBA 2026")
+st.write("Want predict you Common Core GPA?")
+wanted_GPA=st.number_input("Enter the targeted common core GPA: ",
                            min_value = 1.5,
                            max_value = 4.3,
                            value = 3.0)
+tab1, tab2= st.tabs(["Common Core Courses", "GPA"])
+with tab1:
+    subjectT, subjectS, subjectA, subjectH = st.columns(4)
+    with subjectT:
+        st.subheader("T-core")
+        subjectT = st.selectbox("Choose the T-core subject to add: ",
+                        ["ENTR1001", "ENGN1500", "ISOM2400"])
+        Target_Mark_T = st.selectbox("Choose the T-core subject target mark: ",
+                        ["A", "B", "C"])
+    with subjectS:
+        st.subheader("S-core")
+        subjectS = st.selectbox("Choose the S-core subject to add: ",
+                        ["IEDA1522", "PHYS1001", "CHEM2001", "BIOL1001", "SUST2500"])
+        Target_Mark_S = st.selectbox("Choose the S-core subject target mark: ",
+                        ["A", "B", "C"])
+    with subjectA:
+        st.subheader("A-core")
+        subjectA = st.selectbox("Choose the A-core subject to add: ",
+                        ["HUMA1576", "HUMA1011", "HUMA2100"])
+        Target_Mark_A = st.selectbox("Choose the A-core subject target mark: ",
+                        ["A", "B", "C"])
+    with subjectH:
+        st.subheader("H-core")
+        subjectH = st.selectbox("Choose the H-core subject to add: ",
+                        ["HUMA1006", "SOCS2500", "ISOM1001"])
+        Target_Mark_H = st.selectbox("Choose the H-core subject target mark: ",
+                        ["A", "B", "C"])
 
-subjectT, subjectS, subjectA, subjectH = st.columns(4)
-with subjectT:
-    st.subheader("T-core")
-    subjectT = st.selectbox("Choose the T-core subject to add: ",
-                    ["ENTR1001", "ENGN1500", "ISOM2400"])
-    Target_Mark_T = st.selectbox("Choose the T-core subject target mark: ",
-                    ["A", "B", "C"])
-with subjectS:
-    st.subheader("S-core")
-    subjectS = st.selectbox("Choose the S-core subject to add: ",
-                    ["IEDA1522", "PHYS1001", "CHEM2001", "BIOL1001", "SUST2500"])
-    Target_Mark_S = st.selectbox("Choose the S-core subject target mark: ",
-                    ["A", "B", "C"])
-with subjectA:
-    st.subheader("A-core")
-    subjectA = st.selectbox("Choose the A-core subject to add: ",
-                    ["HUMA1576", "HUMA1011", "HUMA2100"])
-    Target_Mark_A = st.selectbox("Choose the A-core subject target mark: ",
-                    ["A", "B", "C"])
-with subjectH:
-    st.subheader("H-core")
-    subjectH = st.selectbox("Choose the H-core subject to add: ",
-                    ["HUMA1006", "SOCS2500", "ISOM1001"])
-    Target_Mark_H = st.selectbox("Choose the H-core subject target mark: ",
-                    ["A", "B", "C"])
+with tab2:
+    st.write("Content for Customer Insights")
+    customer_feedback = [
+        "Great service!",
+        "Very satisfied with the product quality.",
+        "Quick delivery and excellent support."
+    ]
+    for feedback in customer_feedback:
+        st.write(f"- {feedback}")
 
 
 
