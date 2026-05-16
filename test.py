7b8c7ae0f823aea75fe916d4282125020be0bea3
Kseniia-2024 <kgushchina@connect.ust.hk>
2026-05-16 15:56:02 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ st.title("Hong Kong University of Science and Technology 2026")
 st.subheader("Marks Spring Sem")
 st.write("Want predict you final GPA?")
 wanted_GPA=st.number_input("Enter the targeted GPA: ",
-                           min = 1.5,
-                           max=4.3,
-                           value=3.0)
+                           min_value = 1.5,
+                           max_value = 4.3,
+                           value = 3.0)
 
 
 
