3cdfe9c792a55abc7ddc17a9efa875ae936e13e1
Kseniia-2024 <kgushchina@connect.ust.hk>
2026-05-16 16:29:04 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,7 @@ import streamlit as st
 st.title("Hong Kong University of Science and Technology")
 st.header("Common Core Calculator Planner")
 st.subheader("For BBA 2026")
-st.write("Want predict you Common Core GPA?")
-wanted_GPA=st.number_input("Enter the targeted common core GPA: ",
-                           min_value = 1.5,
-                           max_value = 4.3,
-                           value = 3.0)
+
 tab1, tab2= st.tabs(["Common Core Courses", "GPA"])
 with tab1:
     subjectT, subjectS, subjectA, subjectH = st.columns(4)
@@ -37,23 +33,37 @@ with tab1:
                         ["A", "B", "C"])
 
 with tab2:
-    st.write("Content for Customer Insights")
-    customer_feedback = [
-        "Great service!",
-        "Very satisfied with the product quality.",
-        "Quick delivery and excellent support."
+    st.write("Want predict you Common Core GPA?")
+    wanted_GPA=st.number_input("Enter the targeted common core GPA: ",
+                               min_value = 1.5,
+                               max_value = 4.3,
+                               value = 3.0)
+    subjects_list = [
+        {
+          "Subject": subjectT, 
+          "Target Mark": Target_Mark_T
+        },
+        {
+          "Subject": subjectS, 
+          "Target Mark": Target_Mark_S
+        }
+        {
+          "Subject": subjectA, 
+          "Target Mark": Target_Mark_A
+        }
+        {
+          "Subject": subjectH, 
+          "Target Mark": Target_Mark_H
+        }
     ]
-    for feedback in customer_feedback:
-        st.write(f"- {feedback}")
+    for i in subject_list:
+        st.write(f"{i["subject"]}: {i["Target Mark"]}")
 
 
 
 
 
 
-  
-subjects_list = st.selectbox("Choose the subject to add: ",
-                    ["ISOM2010", "ACCT2200", "LANG2068"])
 
 if st.button("Submit"):
     st.success(f"Your wanted GPA: {wanted_GPA}, Total number of subjects: {color}")
